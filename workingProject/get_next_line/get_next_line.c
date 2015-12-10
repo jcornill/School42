7d196e60179cad56cf3f1a81fbb601c13157677c
Julien CORNILLON <jcornill@e3r7p3.42.fr>
2015-12-10 21:28:16 +0100

    Adding GNL

--- workingProject/get_next_line/get_next_line.c
+++ workingProject/get_next_line/get_next_line.c
@@ -6,71 +6,78 @@
 /*   By: jcornill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 17:09:45 by jcornill          #+#    #+#             */
-/*   Updated: 2015/12/08 18:57:42 by jcornill         ###   ########.fr       */
+/*   Updated: 2015/12/10 21:14:13 by jcornill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		process_line(int byte_read, char *buf, char **line, int cursor)
+static int		process_line(int byte_read, char *buf, char **line, int c)
 {
-	int		i;
-	i = 0;
-	while (i < byte_read)
+	int		j;
+	char	*tmp;
+
+	j = 0;
+	while (buf[j] && buf[j] != '\n')
+		j++;
+	if (!(*line))
+		ft_memdel((void **)line);
+	if (c == 0)
+		(*line) = ft_strdup("");
+	if (j)
 	{
-		if (buf[i] == '\n')
-			return (byte_read - i - 1);
-		if (!(*line))
-			if (!((*line) = (char *)malloc(sizeof(char) * (cursor + byte_read))))
-				return (0);
-		(*line)[cursor + i] = buf[i];
-		i++;
-		(*line)[cursor + i] = 0;
+		tmp = ft_strnew(j);
+		ft_strncpy(tmp, buf, j);
+		(*line) = ft_strjoin((*line), tmp);
+		ft_memdel((void **)&tmp);
 	}
-	return (-1);
+	return (byte_read - j - 1);
 }
 
-static int		read_file(int const fd, char **line, char **test)
+static int		process_buf(int *c, char **test, int *cursor, char **line)
+{
+	while (*test)
+	{
+		*c = process_line(ft_strlen(*test), *test, line, *c);
+		(*cursor) += ft_strlen(*test);
+		if (*c > 0)
+		{
+			(*test) = ft_strdup((*test) + (*cursor - *c));
+			return (1);
+		}
+		else if (*c == 0)
+		{
+			*test = NULL;
+			return (1);
+		}
+		*test = NULL;
+	}
+	return (0);
+}
+
+static int		read_file(int const fd, char **line, char **test, int c)
 {
 	int		byte_read;
 	char	buf[BUFF_SIZE + 1];
-	int		cursor;
 	int		char_left;
 
-	cursor = 0;
-	char_left = 0;
-	while (*test)
-	{
-		char_left = process_line(ft_strlen(*test), *test, line, cursor);
-		cursor += ft_strlen(*test);
-		if (char_left > 0)
-		{
-			*test = ft_strdup(&((*test)[cursor - char_left]));
-			return (1);
-		}
-		else if (char_left == 0)
-		{
-			*test = NULL;
-			return (1);
-		}
-		else
-			*test = NULL;
-	}
+	if ((char_left = 0) == 0 && process_buf(&char_left, test, &c, line))
+		return (1);
 	while ((byte_read = read(fd, buf, BUFF_SIZE)))
 	{
-//		printf(" Reading %d", buf[0]);
-		if (byte_read == -1)
+		if ((buf[byte_read] = 0) == 0 && byte_read == -1)
 			return (-1);
-		buf[byte_read] = 0;
-		char_left = process_line(byte_read, buf, line, cursor);
-		cursor += byte_read;
+		char_left = process_line(byte_read, buf, line, c);
+		c += byte_read;
 		if (char_left > 0)
 		{
-			*test = ft_strdup(&buf[byte_read - char_left]);
-			return (1);
+			if (!(*test))
+				ft_memdel((void **)test);
+			(*test) = ft_strnew(char_left);
+			ft_strncpy((*test), buf + (byte_read - char_left), char_left);
 		}
-		else
-			*test = NULL;
+		else if (char_left == 0)
+			ft_memdel((void **)test);
 		if (char_left != -1)
 			return (1);
 	}
@@ -86,7 +93,7 @@ int				get_next_line(int const fd, char **line)
 		(*line) = NULL;
 	else
 		return (-1);
-	if ((read = read_file(fd, line, &test)) == -1)
+	if ((read = read_file(fd, line, &test, 0)) == -1)
 		return (-1);
 	else if (read)
 		return (1);
--- workingProject/get_next_line/get_next_line.h
+++ workingProject/get_next_line/get_next_line.h
@@ -6,17 +6,16 @@
 /*   By: jcornill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 17:11:06 by jcornill          #+#    #+#             */
-/*   Updated: 2015/12/09 18:54:52 by jcornill         ###   ########.fr       */
+/*   Updated: 2015/12/10 19:52:58 by jcornill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10000
 # include <unistd.h>
 # include <stdlib.h>
 # include "libft.h"
-#  include <stdio.h>
 
 int		get_next_line(int const fd, char **line);
 
