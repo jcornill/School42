8882ebc9af791fc695a33394b586f11231ef931f
Julien CORNILLON <jcornill@e3r5p6.42.fr>
2015-12-20 12:39:32 +0100

    Autosaving on rushes

--- workingProject/rushes/alum1/alum1.c
+++ workingProject/rushes/alum1/alum1.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstrprintf.c                                  :+:      :+:    :+:   */
+/*   alum1.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcornill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/16 18:51:05 by jcornill          #+#    #+#             */
-/*   Updated: 2015/12/20 12:20:22 by jcornill         ###   ########.fr       */
+/*   Created: 2015/12/20 12:30:13 by jcornill          #+#    #+#             */
+/*   Updated: 2015/12/20 12:39:29 by jcornill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-
-int		ft_putstrprintf(char *str)
+int		main(int argc, char **argv)
 {
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	write(1, str, ft_strlen(str));
-	return (ft_strlen(str));
+	
 }
