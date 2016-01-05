7d9d4a32b6c6fd67da0c88d22af4be89bd0f5549
Julien CORNILLON <jcornill@e1r9p5.42.fr>
2016-01-05 20:39:17 +0100

    Autosaving on printf

--- workingProject/ft_printf/test.c
+++ workingProject/ft_printf/test.c
@@ -6,7 +6,7 @@
 /*   By: jcornill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/19 16:17:52 by jcornill          #+#    #+#             */
-/*   Updated: 2015/12/19 17:09:46 by jcornill         ###   ########.fr       */
+/*   Updated: 2015/12/22 11:29:11 by jcornill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,5 @@
 
 int main(void)
 {
-	wchar_t *test = NULL;
-	int ret = 0;
-	printf("size: %lu\n", sizeof(wchar_t));
-	//printf("%d-%d-%d-%d\n", *test, *(test + 1), *(test +2), *(test + 3));
-	ret += printf("%S\n", L"Ψ ψ");
-	ret += printf("{%S}\n", test);
-	ft_printf("%d\n", ret);
-	ret = 0;
-	ret += ft_printf("%S\n", L"Ψ ψ");
-	ret += ft_printf("{%S}\n", test);
-	ft_printf("\n%d\n", ret);
+	printf("%+lld\n", -42);
 }
